f0903d866bc33860cc49fb41f80584cacc81c5ee
8Bts <mcmizze@yahoo.com>
2021-08-06 16:22:37 +0400

    Add User model implementation

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save :default_values
+
+  has_and_belongs_to_many :favourites, class_name: 'Item', foreign_key: 'user_id'
+  
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :admin_level, allow_nil: true, numericality: { only_integer: true, greater_than: 0 }
+  
+  private
+
+  def default_values
+    self.admin_level = 0 if self.admin_level.nil?
+  end
+end
