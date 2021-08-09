923cb5896c234888db9822ced77f4c28649db739
8Bts <mcmizze@yahoo.com>
2021-08-09 15:50:42 +0400

    Overwrite as_json of Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :items
 
   validates :name, presence: true, length: { minimum: 2 }
+
+  def as_json(options = {})
+    super(include: :items, except: [:created_at, :updated_at])
+  end
+
 end
