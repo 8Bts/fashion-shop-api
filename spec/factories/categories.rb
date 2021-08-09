dd243566c8f1035af3dcb3abe15fbd855fa544d8
8Bts <mcmizze@yahoo.com>
2021-08-09 14:45:07 +0400

    Add test suite for Category requests

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    name { Faker::Lorem.word }
+    name { Faker::Color.color_name }
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    
+  let!(:categories) { create_list(:category, 10) }
+  let(:category_id) { categories.first.id }
+
+  describe 'GET /categories' do
+    before { get '/categories' }
+
+    it 'returns categories' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /categories/:id' do
+    before { get "/categories/#{category_id}" }
+    context 'when the record exists' do
+      
+      it 'returns the category' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(category_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:category_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Category/)
+      end
+    end
+  end
+
+  describe 'POST /categories' do
+    let(:valid_attributes) { { title: 'T-shirt', price: 200, image: 'https://foo.com' } }
+
+    context 'when the request is valid' do
+      before { post '/categories', params: valid_attributes }
+
+      it 'creates a category' do
+        expect(json['title']).to eq('T-shirt')
+        expect(json['price']).to eq(200)
+        expect(json['image']).to eq('https://foo.com')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/categories', params: { title: 'F' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'PUT /categories/:id' do
+    let(:valid_attributes) { { title: 'T-shirt', price: 300, image: 'https://foo.com' } }
+
+    context 'when the record exists' do
+      before { put "/categories/#{category_id}", params: valid_attributes }
+
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+
+  describe 'DELETE /categories/:id' do
+    before { delete "/categories/#{category_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
   end
 end
