ebbb08cf45bf2a2fcb21ae4b2c4e3fc9aac21a74
8Bts <mcmizze@yahoo.com>
2021-08-09 13:49:27 +0400

    Add implementation of items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+  include Response
+  include ExceptionHandler
+
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    json_response(Item.all)
+  end
+
+  def show
+    json_response(@item)
+  end
+
+  def create
+    @item = Item.create!(items_params)
+    json_response(@item, :created)
+  end
+
+  def update
+    @item.update(items_params)
+    head :no_content
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def items_params
+    params.permit(:title, :price, :image)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :item do
-    title { Faker::Lorem.word }
-    price { Faker::Number.number(range: 30..300) }
+    title { Faker::Lorem.words(number: 2) }
+    price { Faker::Number.within(range: 30..300) }
+    image { Faker::Internet.url }
   end
 end
