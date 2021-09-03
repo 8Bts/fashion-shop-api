b23dd72b821748d8bbca1a1f90d1839cc3247a6b
8Bts <mcmizze@yahoo.com>
2021-09-04 00:00:17 +0400

    Edit routes for add_favourite action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/users/find', to: 'users#find'
-  get '/users/:id', to: 'users#add_favourite'
+  post '/users/:id/favourites', to: 'users#add_favourite'
   resources :users, :items, :categories, only: [:index, :create, :update, :show, :destroy]
 
 end
